052f0f8230753499350cdc1a22e3d75bb211bb1b
jarek <loop123jk@gmail.com>
2026-06-04 21:39:55 -0600

    qdrant_pdfs

---
+++ src/qdrant_pdf/qdrant_pdf.py
@@ -0,0 +1,122 @@
+import hashlib
+import uuid
+from concurrent.futures.thread import ThreadPoolExecutor
+
+from application_controller.app_controller import SimpleApp
+from fsspecc.base_fsspecfs.base_fsspecfs import FSBase
+from index_queue.index_queue import ActionConfig, new_index_queue
+from pfds.pdfm import PDFM, extract_text, extract_images
+from queue_controller.queueData import QueueData
+from thread_safe.index import Index
+from thread_safe.tslist import TsList
+
+from src.qdrantlib.qdrantlib import embed_upload_documents
+
+
+class PDFIndex:
+    _index: Index = None
+    _namespace: str = None
+
+    def __init__(self):
+        self._namespace = "PDFS"
+        self._index = Index().new(self._namespace)
+
+    def store_from_file(self, file: str):
+        self.store_pdf(file, PDFM(pdf_path=file))
+
+    def store_pdf(self, name: str, pdf: PDFM):
+        self._index.store_in_index(self._namespace, name, pdf)
+
+    def load_pdf(self, name) -> PDFM:
+        return self._index.load_from_index(self._namespace, name)
+
+    def list_pdfs(self):
+        return list(self._index.range_index(self._namespace))
+
+    def range_pdfs(self):
+        yield from self._index.range_index(self._namespace)
+
+def uuid5_from_args(namespace: str, *args):
+    ns = uuid.UUID(namespace)
+    return f"{uuid.uuid5(ns, "_".join([str(a) for a in args]))}"
+
+def pdf_pipeline(client, chunk_size, batch_size, worker_count):
+    _tsl = TsList()
+    _namespace = "8c29e160-b99e-4e42-b05d-6c17eb7b9b94"
+    def embed_upload_node(index, queue_data: QueueData):
+        obj = queue_data.attribute("obj")
+        try:
+            embed_upload_documents(client, obj)
+        except Exception as e:
+            raise e
+
+    def buffer_node(index, queue_data: QueueData):
+        obj = queue_data.attributes("obj")
+        _tsl.append(obj)
+        if _tsl.count() > batch_size:
+            index.enqueue("embed_upload_node", str(uuid.uuid4()), _tsl.to_list())
+            _tsl.reset()
+
+    def png_node(index, queue_data: QueueData):
+        pass
+
+    def text_node(index, queue_data: QueueData):
+        key, obj = queue_data.attributes("key", "obj")
+        step_size = chunk_size // 2
+        offset = 0
+        chunk_count = 0
+        while offset < len(obj):
+            chunk = obj[offset : offset + chunk_size]
+
+            is_last_chunk = len(chunk) < chunk_size
+            if is_last_chunk:
+                padding_needed = chunk_size - len(chunk)
+                chunk += b" " * padding_needed
+
+            hsh = hashlib.md5(chunk, usedforsecurity=False).hexdigest()
+
+            document_id = uuid5_from_args(_namespace, key, hsh)
+            index.enqueue("buffer_node", document_id, Document(
+                id=document_id,
+                payload=chunk.decode("utf-8", errors="ignore"),
+                payload_hash=hsh,
+                collection="pdfs", metadata={
+                    "file_name_page": key,
+                    "chunk_number": chunk_count,
+                }))
+
+            if is_last_chunk:
+                break
+            offset += step_size
+            chunk_count += 1
+
+    return new_index_queue(worker_count, text_node, buffer_node, embed_upload_node, png_node)
+
+class PDFIndexQueue:
+    pfi: PDFIndex = None
+    queues: dict[str, ActionConfig] = None
+
+    def __init__(self, client, chunk_size, batch_size, worker_count):
+        self.simple_app = None
+        self.pfi = PDFIndex()
+        self.queues = pdf_pipeline(client, chunk_size, batch_size, worker_count)
+        self.simple_app = SimpleApp(self.queues)
+
+    def init(self, fs: FSBase, path: str):
+        for files in fs.walk(path):
+            for f in files:
+                self.pfi.store_from_file(f"{path}/{f}")
+
+        with ThreadPoolExecutor() as executor:
+            for name, pdf in self.pfi.range_pdfs():
+                extract_text(pdf, executor)
+                extract_images(pdf, executor)
+
+        for filename, value in pdf.range_data():
+            fn = f"{name}_{filename}"
+            if ".txt" in filename:
+                self.simple_app.action_queues.enqueue("text_action", fn, value)
+
+            if ".png" in filename:
+                self.simple_app.action_queues.enqueue("png_node", fn, value)
+
---
+++ src/qdrant_pdf/qdrant_pdf_test.py
@@ -0,0 +1,52 @@
+import asyncio
+import os
+import unittest
+from asyncio import TaskGroup
+
+from application_controller.app_controller import SimpleApp, run_app
+from dotenv import load_dotenv
+from fsspecc.base_fsspecfs.base_fsspecfs import FSBase
+from qdrant_client import QdrantClient
+from qdrantlib.qdrantlib import QdrantBGEM3
+from settings.helper import setting, restore
+
+from hts.hslib.qdrant_pdf.qdrant_pdf import PDFIndexQueue
+
+load_dotenv()
+restore(os.getenv("ENV_FILE"))
+pdfs = FSBase(filesystem="local", path="")
+qdrant_client = QdrantBGEM3(QdrantClient(
+    path="./qdrant_data"),
+    model_cache_dir=setting("LocalAI", "fastembed_cache"))
+
+class MyTestCase(unittest.IsolatedAsyncioTestCase):
+    async def test_something(self):
+        app = PDFIndexQueue(qdrant_client, 1024, 1024, 10)
+        async with TaskGroup() as tg:
+            async def async_wrap():
+                await run_app("PDFS", app.simple_app, "file/settings/settings.yaml")
+            tg.create_task(async_wrap())
+            app.init(pdfs, "./hts/tariffs")
+            await app.simple_app.action_queues.wait_for_completions()
+            print("\n[SUCCESS] 100% of pipeline data has been verified on disk. Exiting process...")
+            return
+
+
+
+async def test_somethinfag(self):
+        query = f"""item:\n  name: Reamer Shoe for Casing Operations\n  material: UNKNOWN\n  mechanical_role: Casing Reamer Shoe for Wellbore Expansion and Stabilization\n  attributes:\n    casing_size: 139.7 mm (5.5 inches)\n    casing_size_fractional: 5-1/2\n    casing_weight_maximum: 26.0 lbs (38.69 kg)\n    casing_weight_minimum: 23.07 lbs (15.5 kg)\n    casing_weight_range: 15.5# - 26.0#\n    connection_up_type: BOX\n    cutting_structure: Tungsten Carbide\n    default_uom: each\n    generic_description: Reamer Shoe\n    long_description: >\n      Works order serial number controls individual bore size for each casing weight.\n      Circulation ports = 3, circulation port size = 30.0 mm (1.181 inches),\n      number of pads = 12, number of gauge blades = 6, gauge length = 173.0 mm (6.813 inches),\n      gauge protection = Tungsten Carbide brickettes, back pressure rating = 10,000 PSI,\n      overall length = 943.0 mm (37.126 inches), set down weight rating = 100,000 LBF\n    material_grade: P110\n    material_specification: Z refer to components\n    material_specification_supplements: Z refer to components\n    model_designation: DIAMONDBACK\n    nose_configuration: Eccentric Aluminum\n    optional_valve: Integral SS3\n    outside_diameter_maximum: 203.2 mm (8.0 inches)\n    pdc_drillability: PDC drillable\n    primary_legacy: RD054080CV\n    reference_documents: D001176110\n    total_flow_area: 3.14 (square inches) / 2025.80 (square mm)"""
+        resp = qdrant_client.fuzzy_query(
+            "pdfs",
+            query,
+            fuzzy_threshold=10.,
+            dense_threshold=.01,
+            sparse_threshold=.01)
+        for text, score in resp:
+            if score > 20.0:
+                print(f"[{score:.1f}% Match] {text}")
+        has_correct_chapter = any("8431" in text or "7304" in text for text, score in resp if score > 20)
+        self.assertTrue(has_correct_chapter, "Failed to locate relevant HTS chapters in vector storage context.")
+
+
+if __name__ == '__main__':
+    unittest.main()
